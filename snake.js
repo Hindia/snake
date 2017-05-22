476b6969f69f3f29476fe814e6ca7244f42453f5
Hindia <mahmmudhindia@gmail.com>
2017-05-23 02:10:44 +0300

    almost there

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 //settings
 var snakeX=2;
 var snakeY=2;
-var height=40;
+var height=30;
 var width=30;
 var interval=100;
 var increment=1;
@@ -12,14 +12,14 @@ var tailY=[snakeY];
 var fX;
 var fY;
 var running=false;
-var gameOver;
-var direction=1;//right 1, left -1, down -2, up +2
+var gameOver=false;
+var direction=1;//right 1, left -1, down -2, up 2
 var int;
 
 //entry point of the game
 function run(){
 	init();
-	int=setInterval(gameloop,interval);
+	int=setInterval(gameLoop,interval);
 }
 
 function init(){
@@ -31,10 +31,10 @@ function init(){
 //generates the map for the snakeX
 function createMap(){
 	document.write("<table>");
-	for(var y=0;y<height;y++){
+	for(var y = 0;y < height;y++){
 		document.write("<tr>");
-		for(var x=0;x<width;x++){
-			if(x==0 || x==width-1 || y==0 || y== height-1)
+		for(var x = 0;x < width;x++){
+			if(x == 0 || x == width-1 || y == 0 || y == height-1)
 				document.write("<td class='wall' id='"+x+ "-" +y+"'></td>");
 			else
 				document.write("<td class='blank' id='"+x+ "-" +y+"'></td>");
@@ -43,27 +43,28 @@ function createMap(){
 	}
 	document.write("</table>");
 }
+
 //generates a snake
 function createSnake(){
 	setType(snakeX,snakeY,"snake");
 }
 
 function get(x,y){
-	return document.getElementById(x+"-"+y);
+	return document.getElementById(x+ "-" +y);
 }
 
 function setType(x,y,value){
-	get(x,y).setAttribute("class", value);
+	if(x != null && y != null) 
+		get(x,y).setAttribute("class",value);
 }
 
 //generates the fruits that pop randomly
 function createFruit(){
-	var found=false;
-	while(!found && (length<(width-2)*(height-2)+1)){
-		var fruitX=rand(1, width-1);
-		var fruitY=rand(1,height-1);
-		if(getType(fruitX,fruitY)=="blank")
-			found=true;
+	var found = false;
+	while(!found && (length < (width-2) * (height-2) + 1)){
+		var fruitX = rand(1, width-1);
+		var fruitY = rand(1,height-1);
+		if(getType(fruitX,fruitY) == "blank") found=true;
 	}
 	setType(fruitX,fruitY,"fruit");
 	fX=fruitX;
@@ -79,5 +80,42 @@ function getType(x,y){
 	
 }
 
+//setting the key listeners
+window.addEventListener("keypress", function key(){
+	//if arrow up is key direction is up and so on
+	var key= KeyboardEvent.code; 
+	if(direction != -2 && key==38) direction=2;
+	else if(direction != 2 && key== 40) direction=-2;
+	else if(direction!= 1 && key == 37)	direction=-1;
+	else if(direction!=-1 && key == 39)	direction=1;
+	if(!running) running=true;
+	else if(key==32) running=false;	
+});
+
+function gameLoop(){
+	if(running && !gameOver)
+		update();
+	else if (gameOver)
+		clearInterval(int);
+}
+
+function update(){
+	setType(fX, fY, "fruit");
+	setType(tailX[length], tailY[length], "blank");
+	if(direction==2) snakeY--;
+	else if (direction==-2) snakeY++;
+	else if (direction==1) snakeX++;
+	else if (direction==-1) snakeX--;
+	setType(snakeX,snakeY,"snake");	
+}
+
+function updateTail(){
+	for(var i=length; i>0 ;i--){
+		tailX[i]=tailX[i-1];
+		tailY[i]=tailY[i-1];
+	}
+	tailX[0]=snakeX;
+	tailY[0]=snakeY;
+}
 
 run();
